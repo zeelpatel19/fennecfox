a2cba59858ea31ab5f02ebcd06257d707cf33d3b
zeelpatel19 <159964414+zeelpatel19@users.noreply.github.com>
2024-02-13 18:46:32 -0500

    final -ver

---
+++ src/utils/constants.js
@@ -0,0 +1,34 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import SchoolIcon from '@mui/icons-material/School';
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+
+
+export const logo = 'https://i.ibb.co/Jv4W0zj/Fennec-Fox-logos.jpg';
+
+export const categories = [
+  { name: 'Music', icon: <MusicNoteIcon /> },
+  { name: 'Education', icon: <SchoolIcon />, },
+  { name: 'Podcast', icon: <GraphicEqIcon />, },
+  { name: 'Movie', icon: <OndemandVideoIcon />, },
+  { name: 'Gaming', icon: <SportsEsportsIcon />, },
+  { name: 'Live', icon: <LiveTvIcon />, },
+  { name: 'Sport', icon: <FitnessCenterIcon />, },
+  { name: 'Fashion', icon: <CheckroomIcon />, },
+  { name: 'Beauty', icon: <FaceRetouchingNaturalIcon />, },
+  { name: 'Comedy', icon: <TheaterComedyIcon />, },
+  { name: 'Gym', icon: <FitnessCenterIcon />, },
+];
+
+export const demoThumbnailUrl = 'https://i.ibb.co/TTptP8M/d-Tf2ys-Bk1-RI-HD.jpg';
+export const demoChannelUrl = '/channel/NoCopyrightSounds';
+export const demoVideoUrl = '/video/K4DyBUG242c';
+export const demoChannelTitle = 'NoCopyrightSounds';
+export const demoVideoTitle = 'Cartoon - On & On (feat. Daniel Levi) | Electronic Pop | NCS - Copyright Free Music';
+export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
